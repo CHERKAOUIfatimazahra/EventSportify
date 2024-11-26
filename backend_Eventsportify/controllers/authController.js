85b0b909035a2cc0322220293e38a62ff5003c9e
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-11-26 10:12:45 +0100

    EP-10 Implémenter l'authentification avec JWT pour les utilisateurs

--- backend_Eventsportify/controllers/authController.js
+++ backend_Eventsportify/controllers/authController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { sendEmail } = require("../utils/mailer");
 const { generateOTP } = require("../utils/otpGenerator");
-// const crypto = require("crypto");
 
 // l'inscription d'un utilisateur
 exports.register = async (req, res) => {
