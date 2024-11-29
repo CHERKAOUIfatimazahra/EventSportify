f6b9d54035c92a70caeebccacbdd7997ae069053
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2024-11-29 17:05:54 +0100

    update packge

--- backend_Eventsportify/server.js
+++ backend_Eventsportify/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth/authRoutes");
+const eventRoutes = require("./routes/event/eventRoute");
+const participantRoutes = require("./routes/event/participantRoute");
 const cors = require("cors");
 
 const app = express();
@@ -19,6 +22,9 @@ app.use(
 app.use(express.json());
 
 // Routes
+app.use("/auth", authRoutes);
+app.use("/events", eventRoutes);
+app.use("/participants", participantRoutes);
 
 
 const PORT = process.env.PORT || 5000;
